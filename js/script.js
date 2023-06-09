c87e329c9b694fef2efae930e40600db3638b74b
Ruhyat Ma'rullah <ruhyatmarullah@gmail.com>
2023-06-09 18:06:35 +0700

    tugas imk

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger menu di klick
+document.querySelector("#humberger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar side bar untuk menghilangkan nav
+const humberger = document.querySelector("#humberger-menu");
+document.addEventListener("click", function (e) {
+  if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
